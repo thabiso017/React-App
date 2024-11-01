9ea25bde5cbda652cf53f47000c9a59c38bcc087
Thabiso Ngwenya <ngwenyats01@gmail.com>
2024-11-01 11:02:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+import App from "./App"
+const react = require("react");
+const ReactDOM = require("ReactDOM");
+
+ReactDOM.render(
+        <div>
+           <App/>      
+        </div>,
+        document.getElementById("root")
+);
